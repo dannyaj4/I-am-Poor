813891be1c44bdf083d89d826ab62b09435a4dc2
Danny A. J <dannyaguswahyudyjeremy@gmail.com>
2020-06-21 16:32:35 +0700

    first commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Danny Aguswahyudy Jeremy on 06/04/20.
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,6 @@
+# I-am-Poor-App
+
+## © Copyright
+Angela Yu - LondonAppBrewery.
+
+From Udemy Online Course.
